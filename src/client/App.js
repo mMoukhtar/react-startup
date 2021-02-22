586c006d625ae055a8609d511ab2edd842c524a1
mMoukhtar <mohamed.moukhtar@outlook.com>
2021-02-22 22:22:39 +0200

    FEAT: add @babel dynamic import syntax

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
+/* 
+----->
+Usually ES6 is only supporting static importing === import has to be at the begining of ES6 file
+This come with a cost that we need to import all components even if we won't use them!
+
+Normal import:
+import Grid from './components/Grid';
+
+Now solution is using the help of @babel dynamic loading syntax plugin
+We use React Lazy with dynamic loading to generate components that is loaded once used
+Finally we need to implement React.Suspense in the routing section
+
+Lazy dynamic import:
+const Grid = React.lazy(() => import('./components/Grid'));
+------>
+*/
+
 class App extends Component {
-    render() {
-        return <div>hello world!</div>;
-    }
+  render() {
+    return <div>hello world!</div>;
+  }
 }
 
 export default App;
--- webpack/configs/webpack.prod.js
+++ webpack/configs/webpack.prod.js
@@ -50,7 +50,7 @@ export default {
           // The point of having it here in case .babelrc is missing
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: [['@babel/plugin-proposal-class-properties']],
+            plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-dynamic-import'],
           },
         },
       },
